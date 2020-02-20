5ec195c5b5a596c71767b14ac7c503bcd53541a7
Elem8100 <49195287+Elem8100@users.noreply.github.com>
2020-02-20 14:51:08 +0800

    ui

---
+++ Src/UI.StatusBar3.pas
@@ -0,0 +1,114 @@
+﻿unit UI.StatusBar3;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Classes, Controls, Forms, Dialogs, Graphics,
+  ACtrlImages, StdCtrls, WZIMGFile, WZArchive, StrUtils, Generics.Collections,
+  WzUtils,  AControls, ACtrlEngine, ACtrlForms, ACtrlButtons, Global,
+  PXT.Canvas, PXT.Graphics, PXT.Types;
+
+type
+  TStatus = class(TAForm)
+  public
+    TargetTexture: TTexture;
+    PosX: Integer;
+    procedure Paint(DC: HDC); override;
+    procedure ReDraw(ReDumpData: Boolean = False);
+    procedure NumberTextout(X, Y: Integer; Number1, Number2: Int64);
+    constructor Create(AOwner: TComponent); override;
+    class procedure CreateUI;
+    class var
+      Instance: TStatus;
+  end;
+
+implementation
+
+uses
+  UI.Utils;
+
+procedure TStatus.Paint(DC: HDC);
+begin
+  var x := ClientLeft;
+  var y := ClientTop;
+  Engine.Canvas.Draw(TargetTexture, x, y);
+end;
+
+procedure TStatus.NumberTextout(X, Y: Integer; Number1, Number2: Int64);
+begin
+  var Char: string;
+  var GraphicNumber := GetImgEntry('UI.wz/StatusBar3.img/mainBar/status/gauge/number');
+  var W: Integer;
+
+  var Middle := -60 + ((Length(Number1.ToString) * 7) + (Length(Number2.ToString) * 7)) div 2;
+  for var I := 1 to Length(Number1.ToString) do
+  begin
+    Char := MidStr(Number1.ToString, I, 1);
+    W := GraphicNumber.Get(Char).Canvas.Width;
+    PosX := PosX + W;
+    GameCanvas.Draw(UIImages[GraphicNumber.Get(Char)], X + PosX - W - Middle, Y);
+  end;
+
+  GameCanvas.Draw(UiImages[GraphicNumber.Get('\')], X + PosX - Middle, Y + 1);
+  for var I := 1 to Length(Number2.ToString) do
+  begin
+    Char := MidStr(Number2.ToString, I, 1);
+    W := GraphicNumber.Get(Char).Canvas.Width;
+    PosX := PosX + W;
+    GameCanvas.Draw(UIImages[GraphicNumber.Get(Char)], 7 + X + posX - W - Middle, Y);
+  end;
+
+end;
+
+procedure TStatus.ReDraw(ReDumpData: Boolean = False);
+begin
+  var Entry := GetImgEntry('UI.wz/StatusBar3.img/mainBar/status');
+  if ReDumpData then
+    DumpData(Entry, UIData, UIImages);
+  Width := 205;
+  Height := 70;
+  PosX := 0;
+  GameCanvas.DrawTarget(TargetTexture, 210, 94,
+    procedure
+    begin
+      GameCanvas.Draw(UIImages[Entry.Get('backgrnd')], 2, 24);
+      GameCanvas.Draw(UIImages[Entry.Get('gauge/hp/layer:0')], 24, 28);
+      GameCanvas.Draw(UIImages[Entry.Get('gauge/mp/layer:0')], 24, 44);
+      GameCanvas.Draw(UIImages[Entry.Get('layer:cover')], -1, 0);
+      NumberTextout(50, 30, 1000002, 3258880022);
+      PosX := 0;
+      NumberTextout(50, 46, 25, 25);
+    end);
+
+end;
+
+constructor TStatus.Create(AOwner: TComponent);
+var
+  Num: Integer;
+begin
+  ControlState := ControlState + [csCreating];
+  inherited Create(AOwner);
+  if (AOwner <> nil) and (AOwner <> Self) and (AOwner is TWControl) then
+  begin
+    Num := 1;
+    while AOwner.FindComponent('Form' + IntToStr(Num)) <> nil do
+      Inc(Num);
+    Name := 'Form' + IntToStr(Num);
+  end;
+  ControlState := ControlState - [csCreating];
+  ReDraw(True);
+end;
+
+class procedure TStatus.CreateUI;
+begin
+  Instance := TStatus.Create(UIEngine.Root);
+  with Instance do
+  begin
+    Left := 300 + 1000;
+    Top := 300 + 1000;
+    CanMove := False;
+  end;
+end;
+
+end.
+
---
+++ Src/UI.Utils.pas
@@ -0,0 +1,422 @@
+unit UI.Utils;
+
+interface
+
+uses
+  Windows, Types, controls, SysUtils, StrUtils, AsphyreSprite, Generics.Collections, WZIMGFile, Math,
+   AbstractTextures, WZArchive, ACtrlEditBoxes, AsphyreTypes, DX9Textures, AControls,
+  ACtrlEngine, ACtrlForms, ACtrlButtons, TypInfo, ACtrlImages, ACtrlDropPanels, ACtrlLabels, Tools,
+  WZDirectory, WZReader, KeyHandler, Global, Classes, AsphyreFonts, ACtrlTypes,PXT.Graphics;
+
+procedure CreateUIs(EntryName: string; X, Y: Integer; wClose: Boolean = True);
+
+function GetPathN(Entry: TWZIMGEntry): string;
+
+procedure CreateButton(EntryName: string; X: Integer = 0; Y: Integer = 0);
+
+procedure CreateButtons(Dir: string; BtName: array of string; X: Integer = 0; Y: Integer = 0);
+
+procedure CreateButtonAll(EntryName: string; IgnoreDir: array of string; X, Y: Integer);
+
+procedure CreateImages(Dir: string; ImageName: array of string; X: Integer = 0; Y: Integer = 0);
+
+procedure CreateForm(EntryName: string; X, Y: Integer; BtClose: Boolean = True);
+
+procedure CreateFormEx(EntryName: string; X, Y: Integer);
+
+procedure CreateEdit(EntryName: string; X, Y, AWidth: Integer);
+
+procedure CreateLabel(EntryName, AText: string; X, Y: Integer; AWidth: Integer = 100; Black: Boolean = True);
+
+procedure CreateImage(EntryName: string; X: Integer = 0; Y: Integer = 0);
+
+procedure CreateGrid(AImagePath: string; X, Y, Col, Row: Integer; OwnerName: string);
+
+procedure CreateDummy(W, H, X, Y: Integer);
+
+procedure ShowForm(Name: string);
+
+function HasUI(Key: TWZIMGEntry): Boolean;
+
+var
+
+  UIImages: TObjectDictionary<TWZIMGEntry, TTexture>;
+  UIData: TObjectDictionary<string, TWZIMGEntry>;
+  UIButton: TDictionary<string, TAButton>;
+  UIForm: TDictionary<string, TAForm>;
+  UIImage: TDictionary<string, TAImage>;
+  UILabel: TDictionary<string, TALabel>;
+ // UITab: TDictionary<string, TUITab>;
+  UIOwner: string;
+
+implementation
+
+uses
+   WzUtils, ColorUtils,minimap;
+
+
+function GetPathN(Entry: TWZIMGEntry): string;
+var
+  Path: string;
+  E: TWZEntry;
+begin
+  Path := Entry.Name;
+  E := Entry.Parent;
+  while E <> nil do
+  begin
+    Path := E.Name + '/' + Path;
+    E := E.Parent;
+  end;
+  Result := StringReplace(Path, '.wz', '', [rfReplaceAll]);
+end;
+
+function TrimS(Stemp: string): string;
+const
+  Remove =['/', #13, #10];
+var
+  I: Integer;
+begin
+  Result := '';
+  for I := 1 to Length(Stemp) do
+  begin
+    if not (Stemp[I] in Remove) then
+      Result := Result + Stemp[I];
+  end;
+end;
+
+function ToPath(Name: string): string;
+var
+  S: string;
+begin
+  S := StringReplace(Name, '_img', '.img', [rfReplaceAll]);
+  Result := StringReplace(S, '_', '/', [rfReplaceAll]);
+end;
+
+function ToName(S: string): string;
+begin
+  while (Pos('/', S) > 0) do
+    S[Pos('/', S)] := '_';
+  while (Pos('.', S) > 0) do
+    S[Pos('.', S)] := '_';
+
+  Result := S;
+end;
+
+function GetImg(Path: string): string;
+begin
+  Result := LeftStr(Path, Pos('/', Path) - 1) + '.img';
+end;
+
+function GetPathS(Path: string): string;
+begin
+  Result := MidStr(Path, Pos('/', Path) + 1, 100);
+end;
+
+function HasUI(Key: TWZIMGEntry): Boolean;
+begin
+  Result := UIImages.ContainsKey(Key);
+end;
+
+procedure CreateForm(EntryName: string; X, Y: Integer; BtClose: Boolean = True);
+var
+  Entry: TWZIMGEntry;
+  Form: TAForm;
+  Ax: Integer;
+begin
+  Entry := GetImgEntry(EntryName);
+  DumpData(Entry, UIData, UIImages);
+
+  Form := TAForm.Create(UIEngine.Root);
+
+  with Form do
+  begin
+    ImageEntry := Entry;
+    Name := ToName(EntryName);
+    UIOwner := Name;
+    Width := Entry.Canvas.Width;
+    Height := Entry.Canvas.Height;
+    Left := X + -Entry.Child['origin'].Vector.X + 1000;
+    Top := Y + -Entry.Child['origin'].Vector.Y + 1000;
+    BorderWidth := 0;
+    ShadowWidth := 0;
+    Ax := Width - 22;
+  end;
+  if BtClose then
+    CreateButton('UI/Basic.img/BtClose3', Ax, 7);
+  UIForm.Add(EntryName, Form);
+end;
+
+procedure CreateFormEx(EntryName: string; X, Y: Integer);
+var
+  Entry: TWZIMGEntry;
+  Form: TAForm;
+begin
+  Entry := GetImgEntry(EntryName);
+  DumpData(Entry, UIData, UIImages);
+  Form := TAForm.Create(UIEngine.AForm(UIOwner));
+  with Form do
+  begin
+    ImageEntry := Entry;
+    Name := ToName(Entry.GetPath);
+    Width := Entry.Canvas.Width;
+    Height := Entry.Canvas.Height;
+    Left := X + -Entry.Child['origin'].Vector.X;
+    Top := Y + -Entry.Child['origin'].Vector.Y;
+    CanMove := False;
+    BorderWidth := 0;
+    ShadowWidth := 0;
+  end;
+  UIForm.Add(EntryName, Form);
+end;
+
+procedure CreateButton(EntryName: string; X: Integer = 0; Y: Integer = 0);
+var
+  Entry: TWZIMGEntry;
+  Button: TAButton;
+begin
+  Entry := GetImgEntry(EntryName);
+  DumpData(Entry, UIData, UIImages);
+
+  Button := TAButton.Create(UIEngine.AForm(UIOwner));
+  with Button do
+  begin
+    ImageEntry := Entry.Get('normal/0');
+    Width := Entry.Get('normal/0').Canvas.Width;
+    Height := Entry.Get('normal/0').Canvas.Height;
+    Left := X + -Entry.Get('normal/0/origin').Vector.X;
+    Top := Y + -Entry.Get('normal/0/origin').Vector.Y;
+    Name := ToName(EntryName);
+    ImageHover := Entry.Get('mouseOver/0');
+    ImagePressed := Entry.Get('pressed/0');
+    ImageDisabled := Entry.Get('disabled/0');
+  end;
+  UIButton.AddOrSetValue(EntryName, Button);
+end;
+
+procedure CreateButtons(Dir: string; BtName: array of string; X: Integer = 0; Y: Integer = 0);
+var
+  I: Integer;
+begin
+  for I := 0 to High(BtName) do
+    CreateButton(Dir + BtName[I], X, Y);
+end;
+
+procedure CreateImage(EntryName: string; X: Integer = 0; Y: Integer = 0);
+var
+  Entry: TWZIMGEntry;
+  Image: TAImage;
+begin
+  Entry := GetImgEntry(EntryName);
+  DumpData(Entry, UIData, UIImages);
+  Image := TAImage.Create(UIEngine.AForm(UIOwner));
+  with Image do
+  begin
+    ImageEntry := Entry;
+    Width := Entry.Canvas.Width;
+    Height := Entry.Canvas.Height;
+    Left := X + -Entry.Child['origin'].Vector.X;
+    Top := Y + -Entry.Child['origin'].Vector.Y;
+    CanMoveHandle := False;
+    Name := ToName(EntryName);
+   // OnMouseDown := Event.OnMouseDown;
+   // OnMouseUp := Event.OnMouseUp;
+    BorderWidth := 0;
+  end;
+  UIImage.AddOrSetValue(EntryName, Image);
+end;
+
+procedure CreateImages(Dir: string; ImageName: array of string; X: Integer = 0; Y: Integer = 0);
+var
+  I: Integer;
+begin
+  for I := 0 to High(ImageName) do
+    CreateImage(Dir + ImageName[I], X, Y);
+end;
+
+procedure CreateUIs(EntryName: string; X, Y: Integer; wClose: Boolean = True);
+var
+  Entry, Iter: TWZIMGEntry;
+  Ax: Integer;
+  FormEntry: string;
+begin
+  Entry := GetImgEntry(EntryName);
+
+  for Iter in Entry.Children do
+  begin
+    if Iter.Name = 'backgrnd' then
+    begin
+      FormEntry := GetPathN(Iter);
+      CreateForm(FormEntry, X, Y, wClose);
+    end;
+    if (Iter.Name = 'backgrnd1') or (Iter.Name = 'backgrnd2') or (Iter.Name = 'backgrnd3') then
+      CreateImage(GetPathN(Iter));
+
+    if LeftStr(Iter.Name, 2) = 'Bt' then
+      CreateButton(GetPathN(Iter));
+
+  end;
+
+end;
+
+procedure CreateButtonAll(EntryName: string; IgnoreDir: array of string; X, Y: Integer);
+var
+  Entry, Iter: TWZIMGEntry;
+  I: Integer;
+begin
+  Entry := GetImgEntry(EntryName);
+
+  for Iter in Entry.Children do
+    if (LeftStr(Iter.Name, 2) = 'Bt') then
+    begin
+      case High(IgnoreDir) of
+        0:
+          if (Iter.Name = IgnoreDir[0]) then
+            Continue;
+        1:
+          if (Iter.Name = IgnoreDir[0]) or (Iter.Name = IgnoreDir[1]) then
+            Continue;
+        2:
+          if (Iter.Name = IgnoreDir[0]) or (Iter.Name = IgnoreDir[1]) or (Iter.Name = IgnoreDir[2]) then
+            Continue;
+        3:
+          if (Iter.Name = IgnoreDir[0]) or (Iter.Name = IgnoreDir[1]) or (Iter.Name = IgnoreDir[2])
+            or (Iter.Name = IgnoreDir[3]) then
+            Continue;
+      end;
+      CreateButton(GetPathN(Iter), X, Y);
+    end;
+
+end;
+
+procedure CreateEdit(EntryName: string; X, Y, AWidth: Integer);
+begin
+  with TAEditBox.Create(UIEngine.AForm(UIOwner)) do // UIEngine.AForm(ParentName)
+  begin
+    Color.SetFillColor(cRGB4(0, 0, 0, 0));
+   // Zfont := ZFonta;
+    BorderWidth := 0;
+    Left := X;
+    Top := Y;
+    Width := AWidth;
+    Height := 40;
+    Name := ToName(EntryName);
+
+    SelStart := 0;
+  end;
+end;
+
+procedure CreateLabel(EntryName: string; AText: string; X, Y: Integer; AWidth: Integer = 100; Black: Boolean = True);
+var
+  ALabel: TALabel;
+begin
+  ALabel := TALabel.Create(UIEngine.AForm(UIOwner));
+  with ALabel do // UIEngine.AForm(ParentName)
+  begin
+    Color.SetFillColor(cRGB4(0, 0, 0, 0));
+  //  Zfont := ZFontb;
+  //  if Black then
+    //  FontColor.SetFontColor(cRGB1(85, 85, 85), cRGB1(85, 85, 85))
+    //else
+    //  FontColor.SetFontColor($FFFF0000, $FFFF0000);
+    BorderWidth := 0;
+    Left := X;
+    Top := Y;
+    Width := AWidth;
+    Height := 17;
+    Text := AText;
+    CanMoveHandle := False;
+    TextHorizontalAlign := hLeft;
+    Name := ToName(EntryName);
+  end;
+  UILabel.Add(EntryName, ALabel);
+end;
+
+procedure CreateGrid(AImagePath: string; X, Y, Col, Row: Integer; OwnerName: string);
+var
+  I: Integer;
+  Entry: TWZIMGEntry;
+  // IDs:array[0..5] of string=('0100100','0100101','01');
+begin
+
+  for I := 1000000 to 1000010 do
+  begin
+    Entry := CharacterWZ.GetImgFile('Cap/' + '0' + IntToStr(I) + '.img').Root.Get('info/icon');
+    UIImages.Add(Entry, Entry.Canvas.Dump(ceNone, 0));
+
+  end;
+
+  with TADropPanel.Create(UIEngine.AForm(ToName(OwnerName))) do // UIEngine.AForm(ParentName)
+  begin
+    //
+    // ImageName := AImageName;
+    // itemcolor.SetFillColor(crgb4(0,0,0,0));
+
+    RowWidth := 30;
+    RowHeight := 30;
+    Name := ToName(AImagePath);
+    Color.SetFillColor(cRGB4(250, 0, 0, 0));
+    BorderWidth := 1;
+    Rows := Row;
+    Columns := Col;
+    Left := X;
+    Top := Y;
+    // Margin:=5;
+    Items[1].ID := 105;
+    // Items[1].Image :='01000001';
+    Items[2].ID := 105;
+    // Items[2].Image :='01000005';
+  end;
+end;
+
+procedure CreateDummy(W, H, X, Y: Integer);
+var
+  Entry: TWZIMGEntry;
+  Form: TAForm;
+  Ax: Integer;
+  fColor: tfillcolor;
+begin
+
+  fColor := tfillcolor.Create;
+ //fcolor.SetFillColor(200,100,100,100);
+  Form := TAForm.Create(UIEngine.Root);
+  with Form do
+  begin
+    ImageEntry := nil;
+    //Name := 'c';//ToName(GetPathN(Entry));
+    //UIOwner := Name;
+    Width := W; //Entry.Canvas.Width;
+    Height := H; //Entry.Canvas.Height;
+    Left := X; //X + -Entry.Child['origin'].Vector.X + 1000;
+    Top := Y; //Y + -Entry.Child['origin'].Vector.Y + 1000;
+    BorderWidth := 0;
+    ShadowWidth := 0;
+    Color := fColor;
+   // Ax := Width - 22;
+  end;
+  fColor.Free;
+end;
+
+procedure ShowForm(Name: string);
+begin
+  UIForm[Name].BringToFront;
+  UIForm[Name].Visible := not UIForm[Name].Visible;
+end;
+
+var
+  MouseDownPosY: Integer;
+  DragEnabled: Boolean;
+
+
+initialization
+   UIData := TObjectDictionary<string, TWZIMGEntry>.Create;
+  UIImages := TObjectDictionary<TWZIMGEntry, TTexture>.Create;
+  UIButton := TDictionary<string, TAButton>.Create;
+  UIForm := TDictionary<string, TAForm>.Create;
+  UIImage := TDictionary<string, TAImage>.Create;
+  UILabel := TDictionary<string, TALabel>.Create;
+  //UITab := TDictionary<string, TUITab>.Create;
+
+end.
+
+
+
